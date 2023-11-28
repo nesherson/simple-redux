76b540fdcfffb4dab30a4b6af4d4f55a9df200d1
Nešad Omeragić <nesad.omeragich27@gmail.com>
2023-11-28 11:26:55 +0100

    Add initial structure

---
+++ index.js
@@ -0,0 +1,35 @@
+const initialState = {
+    value: 0,
+  };
+  
+  function counterReducer(state = initialState, action) {
+    switch (action.type) {
+      case "counter/incremented":
+        return { ...state, value: state.value + 1 };
+      case "counter/decremented":
+        return { ...state, value: state.value - 1 };
+      default:
+        return state;
+    }
+  }
+  
+  const store = Redux.createStore(counterReducer);
+  
+  const valueEl = document.getElementById("value");
+  
+  function render() {
+    const state = store.getState();
+    valueEl.innerHTML = state.value.toString();
+  }
+  
+  render();
+  store.subscribe(render);
+  
+  document.getElementById("increment").addEventListener("click", function () {
+    store.dispatch({ type: "counter/incremented" });
+  });
+  
+  document.getElementById("decrement").addEventListener("click", function () {
+    store.dispatch({ type: "counter/decremented" });
+  });
+  
