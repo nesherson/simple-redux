13629fdb0cc4e744de80c25ed78208244d082b39
Nešad Omeragić <nesad.omeragich27@gmail.com>
2025-05-05 14:56:38 +0200

    Add support for multiple middlewares

--- index.js
+++ index.js
@@ -1,48 +1,61 @@
 const initialState = {
-    value: 0,
-  };
-  
-  function counterReducer(state = initialState, action) {
-    switch (action.type) {
-      case "counter/incremented":
-        return { ...state, value: state.value + 1 };
-      case "counter/decremented":
-        return { ...state, value: state.value - 1 };
-      default:
-        return state;
-    }
-  }
+	value: 0,
+};
 
-  function logger({getState, dispatch}) {
-    return (action) => {
-        console.log("Will dispatch -> ", action);
+function counterReducer(state = initialState, action) {
+	switch (action.type) {
+		case "counter/increment":
+			return { ...state, value: state.value + 1 };
+		case "counter/decrement":
+			return { ...state, value: state.value - 1 };
+		default:
+			return state;
+	}
+}
 
-        const returnValue = dispatch(action);
+function loggerInfo({ getState }) {
+	return (next) => (action) => {
+		console.log("loggerInfo/will dispatch", action);
 
-        console.log("State after dispatch", getState());
+		const returnValue = next(action);
 
-        return returnValue;
-    };
-  }
-  
-//   const store = Redux.createStore(counterReducer);
-  const store = SimpleRedux.createStore(counterReducer, SimpleRedux.applyMiddleware(logger));
-  
-  const valueEl = document.getElementById("value");
-  
-  function render() {
-    const state = store.getState();
-    valueEl.innerHTML = state.value.toString();
-  }
-  
-  render();
-  store.subscribe(render);
-  
-  document.getElementById("increment").addEventListener("click", function () {
-    store.dispatch({ type: "counter/incremented" });
-  });
-  
-  document.getElementById("decrement").addEventListener("click", function () {
-    store.dispatch({ type: "counter/decremented" });
-  });
-  
+		console.log("loggerInfo/state after dispatch", getState());
+
+		return returnValue;
+	};
+}
+
+function loggerWarning({ getState }) {
+	return (next) => (action) => {
+		console.warn("loggerWarning/will dispatch", action);
+
+		const returnValue = next(action);
+
+		console.warn("loggerWarning/state after dispatch", getState());
+
+		return returnValue;
+	};
+}
+
+const store = SimpleRedux.createStore(
+	counterReducer,
+	SimpleRedux.applyMiddleware(loggerInfo, loggerWarning),
+);
+
+const valueEl = document.getElementById("value");
+
+function render() {
+	const state = store.getState();
+	valueEl.innerHTML = state.value.toString();
+}
+
+render();
+store.subscribe(render);
+
+document.getElementById("increment").addEventListener("click", () => {
+	store.dispatch({ type: "counter/increment" });
+});
+
+document.getElementById("decrement").addEventListener("click", () => {
+	store.dispatch({ type: "counter/decrement" });
+});
--- simpleRedux.js
+++ simpleRedux.js
@@ -1,39 +1,55 @@
 function createStore(reducer, enhancer) {
-    if (enhancer) {
-        return enhancer(createStore)(reducer);
-    }
+	if (enhancer) {
+		return enhancer(createStore)(reducer);
+	}
 
+	let state = reducer(undefined, { type: "@@INIT" });
+	const listeners = [];
 
-    let state = reducer(undefined, { type: "@@INIT" });
-    const listeners = [];
-
-    return {
-        dispatch: (action) => {
-            state = reducer(state, action);
-            listeners.forEach(listener => listener());
-        },
-        subscribe: (listener) => {
-            listeners.push(listener);
-        },
-        getState: () => state
-    };
+	return {
+		dispatch: (action) => {
+			state = reducer(state, action);
+			for (const listener of listeners) {
+				listener();
+			}
+		},
+		subscribe: (listener) => {
+			listeners.push(listener);
+		},
+		getState: () => state,
+	};
 }
 
-function applyMiddleware(middleware) {
-    return (createStore) => (reducer) => {
-        const store = createStore(reducer);
+function applyMiddleware(...middlewares) {
+	return (createStore) => (reducer) => {
+		const store = createStore(reducer);
+		const middlewareApi = {
+			getState: store.getState,
+			dispatch: (action, ...args) => dispatch(action, ...args),
+		};
+		const dispatchChain = middlewares.map((middleware) =>
+			middleware(middlewareApi),
+		);
+		const composedMiddleware = compose(...dispatchChain);
+		const dispatch = composedMiddleware(store.dispatch);
 
-        return {
-            ...store,
-            dispatch: middleware({
-                getState: store.getState,
-                dispatch: store.dispatch
-            })
-        };
-    }
+		return {
+			...store,
+			dispatch,
+		};
+	};
+}
+
+function compose(...functions) {
+	if (functions.length === 0) {
+		return (arg) => arg;
+	}
+
+	return functions.reduce((a, b) => (arg) => a(b(arg)));
 }
 
 const SimpleRedux = {
-    createStore,
-    applyMiddleware   
+	createStore,
+	applyMiddleware,
+	compose,
 };
